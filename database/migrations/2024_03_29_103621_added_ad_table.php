114d137391e50f31f767dedabfb2e298c0e6a0bc
Tim Bogers <timbogers@hotmail.nl>
2024-03-29 11:40:18 +0100

    Migration aangepast

--- database/migrations/2024_03_29_103621_added_ad_table.php
+++ database/migrations/2024_03_29_103621_added_ad_table.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->double('price');
+            $table->string('type');
+        });
     }
 
     /**
@@ -19,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('ads');
     }
 };
