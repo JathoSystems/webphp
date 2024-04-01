2213e6ee18675ac2d214a85e1442021cd686f9af
Tim Bogers <timbogers@hotmail.nl>
2024-04-01 14:41:09 +0200

    Removed styling from head tag

--- resources/views/contracts/index.blade.php
+++ resources/views/contracts/index.blade.php
@@ -4,11 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{__("Contracts")}}</title>
-    <style>
-        svg{
-            height:20px;
-        }
-    </style>
 </head>
 <body>
     <x-navbar />
